841047a83b5e98230d587d3049451753cdadb86b
lj259 <120034783@upq.edu.mx>
2025-05-20 21:49:46 -0600

    Ejercicio 3 - Practica 2

---
+++ Practica2/Ejercicio3.js
@@ -0,0 +1,15 @@
+const personas = [
+    {nombre: "Ana", edad: 22},
+    {nombre: "Luis", edad: 35},
+    {nombre: "María", edad: 28},
+];
+
+const find = personas.find(persona => persona.nombre === "Luis");
+console.log(find);
+
+personas.forEach(persona => {
+    console.log("Nombre: "+persona.nombre+"Edad: "+persona.edad)
+});
+
+const reduce = personas.reduce((acumulador,persona) => acumulador + persona.edad,0);
+console.log(reduce);
